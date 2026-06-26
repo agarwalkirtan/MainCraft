f5e3fd7bd7ddd5f724c4acb0dd99edb908509bb2
kirtan agarwal <kirtanagrwal@gmail.com>
2026-06-26 23:29:04 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function validateForm(){
+
+    let name =
+    document.forms["contactForm"]["name"].value;
+
+    let email =
+    document.forms["contactForm"]["email"].value;
+
+    if(name === "" || email === ""){
+        alert("Name and Email must be filled out!");
+        return false;
+    }
+
+    alert("Form Submitted Successfully!");
+    return true;
+}
